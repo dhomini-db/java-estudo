7407c3d1b8de8e7f314a6ebd95a2b85bdcbb9d4e
Dhomini De Bom <dbomdhomini@gmail.com>
2026-02-26 21:33:56 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,26 +1,21 @@
-import java.util.Locale;
+
 import java.util.Scanner;
-public class Main {
-    public static void main(String[] args){
-        Scanner sc =new Scanner(System.in);
-        Locale.setDefault(Locale.US);
+public class Main{
+    void main(String [] args ){
+        Scanner sc = new Scanner(System.in);
 
         String nome;
-        int idade;
-
-        System.out.print("digite seu nome:");
+        System.out.print("digite seu nome heitor:");
         nome = sc.next();
 
-        System.out.printf("ola %s%n",nome);
+        while(true){
 
-        System.out.print("digite sua idade:");
-        idade = sc.nextInt();
-
-
-        if(idade >= 2 && idade == 4){
-            System.out.print("parabens");
-        } else {
-            System.out.println("nao parabens");
+            if(nome.equalsIgnoreCase("heitor")){
+                System.out.print("parabens heitor");
+            } else{
+                System.out.printf("vc nao é o %s",nome);
+                break;
+            }
         }
 
         sc.close();
